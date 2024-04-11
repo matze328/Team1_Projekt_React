7c2ebc555ef1b3757d6d99f2ae878e8414423307
matze328 <152269645+matze328@users.noreply.github.com>
2024-04-11 10:14:44 +0200

    navbar Logo update
    onClick function  /home Route hinterlegt
      - cursor: pointer; in der CSS Datei hinzugefügt

--- src/components/layout/navbar/navbar-left/NavBarLeft.js
+++ src/components/layout/navbar/navbar-left/NavBarLeft.js
@@ -1,9 +1,15 @@
 import styles from "./NavBarLeft.module.css";
 import logo from "../../../../images/mutrix.jpg";
 
+
+
 function NavBarLeft() {
+  const redirectToHomepage = () => {
+    window.location.href = "http://localhost:3000/"
+
+  };
   return (
-    <div className={styles.mainContainer}>
+    <div className={styles.mainContainer} onClick={redirectToHomepage}>
       <img className={styles.logImg} src={logo} alt="logo" />
 
       <div className={styles.appName}> MUTRIX</div>
