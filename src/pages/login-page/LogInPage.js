29a84cde69d2ab1a0023a8113ef24118c69bf13a
matze328 <152269645+matze328@users.noreply.github.com>
2024-04-11 13:39:12 +0200

    login form erstellt
    anmelde form erstellt

--- src/pages/login-page/LogInPage.js
+++ src/pages/login-page/LogInPage.js
@@ -1,7 +1,34 @@
 import styles from "./LogInPage.module.css";
+import { useState } from "react";
 
 function LogInPage() {
-  return <div className={styles.mainContainer}> This is my LogInPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
+
+  const handleLogin = (e) => {
+      e.preventDefault();
+      // Hier können Sie die Logik für die Anmeldung implementieren, z.B. API-Aufruf zur Überprüfung der Anmeldeinformationen
+
+  
+  };
+
+
+  return (
+    <div className={styles.mainContainer}>
+    <form onSubmit={handleLogin}>
+        <label>Email: </label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <br />
+        <label>Password: </label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <br />
+        <button type="submit">Login</button>
+    </form>
+    </div>
+);
 }
+
+
 
 export default LogInPage;
