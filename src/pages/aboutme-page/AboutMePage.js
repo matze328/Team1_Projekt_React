35692c61602ff0ea2f50e8a0537d2b0fb5991e95
h4n5o <72876852+h4n5o@users.noreply.github.com>
2024-04-16 13:35:32 +0200

    About me Page Anordnung

--- src/pages/aboutme-page/AboutMePage.js
+++ src/pages/aboutme-page/AboutMePage.js
@@ -1,7 +1,26 @@
 import styles from "./AboutMePage.module.css";
 
 function AboutMePage() {
-  return <div className={styles.mainContainer}> This is about me</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.unterContainer}>
+        <img className={styles.profileImage} src="person1.jpg" alt="Person 1" />
+        <p>Person 1 - This is about me</p>
+      </div>
+      <div className={styles.unterContainer}>
+        <img className={styles.profileImage} src="person2.jpg" alt="Person 2" />
+        <p>Person 2 - This is about me</p>
+      </div>
+      <div className={styles.unterContainer}>
+        <img className={styles.profileImage} src="person3.jpg" alt="Person 3" />
+        <p>Person 3 - This is about me</p>
+      </div>
+      <div className={styles.unterContainer}>
+        <img className={styles.profileImage} src="person4.jpg" alt="Person 4" />
+        <p>Person 4 - This is about me</p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMePage;
