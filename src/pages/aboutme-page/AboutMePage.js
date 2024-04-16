38a4985f3b1072e9989e4422ef97dba4b0bd7a7b
matze328 <152269645+matze328@users.noreply.github.com>
2024-04-16 14:14:09 +0200

    update aboutMePage und Box
    style angepasst AboutMeBox  und aboutMePage

--- src/pages/aboutme-page/AboutMePage.js
+++ src/pages/aboutme-page/AboutMePage.js
@@ -3,23 +3,34 @@ import React from "react";
 import AboutMeBox from "../../components/common/aboutMeBox";
 function AboutMePage() {
   return (
-    <div>  className={styles.mainContainer}
-    <div> className={styles.unterContainer}
-    <AboutMeBox 
-     vorname ="Marcel"
-     nachname="Büscher"
-     persoInfo= "Ich bin ein leidenschaftlicher Entwickler und liebe es, kreative Lösungen zu finden."
-     hobbies="In meiner Freizeit fahre ich Downhill und Freeride."
-     />,
-   <div>  className={styles.unterContainer}
+   <div className={styles.mainContainer}>  
      <AboutMeBox 
      vorname ="Marcel"
      nachname="Büscher"
      persoInfo= "Ich bin ein leidenschaftlicher Entwickler und liebe es, kreative Lösungen zu finden."
      hobbies="In meiner Freizeit fahre ich Downhill und Freeride."
      />
- </div>   </div> </div>
+     
+       <AboutMeBox    
+        vorname="Person 2"
+        nachname="This is about me"
+        hobbies="Hobby 3, Hobby 4"
+      />
+      <AboutMeBox
+        vorname="Person 3"
+        nachname="This is about me"
+        hobbies="Hobby 5, Hobby 6"
+      />
+      <AboutMeBox
+        vorname="Person 4"
+        nachname="This is about me"
+        hobbies="Hobby 7, Hobby 8"
+      />
+    </div>
   );
 }
+
+
+
 
 export default AboutMePage;
