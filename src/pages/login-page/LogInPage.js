041daf92cead4911220b8f8402750e9274c4a398
matze328 <152269645+matze328@users.noreply.github.com>
2024-04-24 12:06:51 +0200

    init Loginund signup mit backend
    LogIn Route mit Datenbank verbunden
    SignUp Route mit Datenbank Verbunden

--- src/pages/login-page/LogInPage.js
+++ src/pages/login-page/LogInPage.js
@@ -3,11 +3,12 @@ import UserContext from "../../components/common/userContext";
 import styles from "./LogInPage.module.css";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthQueries } from "../../api/v1/auth";
 
 // import { useHistory } from "react-router-dom";
 
 function LogInPage() {
-  const [username, setUsername] = useState("");
+  const [userName, setUsername] = useState("");
 
   const [password, setPassword] = useState("");
   const { users } = useContext(UserContext);
@@ -16,23 +17,20 @@ function LogInPage() {
 
   console.log(users);
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     // Hier können Sie die Logik für die Anmeldung implementieren, z.B. API-Aufruf zur Überprüfung der Anmeldeinformationen
     // history.push("/home");
-    const founduser = users.find((user) => user.username === username);
-    if (founduser) {
-      if (password === founduser.password) {
-        console.log("Du bist angemeldet.");
-        navigate("/home");
-        // hier: navigiere zu Homepage
-      } else {
-        console.log("FEHLER: PASSWORD");
-      }
-    } else {
-      console.log("FEHLER: USER");
-    }
-  };
+    try {
+    const token = await AuthQueries.fetchlogin(userName, password);
+    console.log("du bist angemeldet",token)
+    navigate("/home"); // Navigiere zur Homepage nach erfolgreicher Anmeldung
+  } catch (error) {
+    console.error("Fehler beim Einloggen:", error);
+    // Hier können Sie entsprechende Fehlermeldungen anzeigen
+  }
+};
+
   const handleCancel = () => {
     window.location.href = "http://localhost:3000/";
   };
@@ -44,9 +42,9 @@ function LogInPage() {
         <input
           className={styles.input}
           onSubmit={handleLogin}
-          placeholder="Username"
-          type="username"
-          value={username}
+          placeholder="UserName"
+          type="userName"
+          value={userName}
           onChange={(e) => setUsername(e.target.value)}
           required
         />
--- src/pages/signup-page/SignUpPage.js
+++ src/pages/signup-page/SignUpPage.js
@@ -1,21 +1,39 @@
 import StandardBtn from "../../components/common/buttons/standard-btn";
 import styles from "./SignUpPage.module.css";
-
 import React, { useState } from "react";
 
+import AuthMutations from "../../api/v1/auth/AuthMutations";
 function SignUpPage() {
-  const [username, setUsername] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [newUserName, setNewUserName] = useState("");
+  const [newVorName, setNewVorName] = useState("");
+  const [newNachName, setNewNachName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const handleConfirm = () => {
-    window.location.href = "http://localhost:3000/user";
-  };
+  // const handleConfirm = () => {
+  //   window.location.href = "http://localhost:3000/user";
+  // };
 
   const handleCancel = () => {
     window.location.href = "http://localhost:3000/";
+  };
+
+  const handleSignup = async () => {
+    if (newPassword !== confirmPassword) {
+      setError('Passwörter stimmen nicht überein');
+      return;
+    } 
+      try {
+        const profile = await AuthMutations.createUser(newUserName, newVorName, newNachName, newEmail, newPassword);
+        console.log('Benutzer erfolgreich registriert:', profile);
+        window.location.href = "http://localhost:3000/user"; // Weiterleitung zur Benutzerseite
+      } catch (error) {
+        console.error('Fehler beim Registrieren:', error);
+        // Hier können Sie eine Fehlermeldung anzeigen
+      }
+    
   };
 
   return (
@@ -23,20 +41,21 @@ function SignUpPage() {
     <div className={styles.innerContainer}>
     <br />
      <label>Bitte gib hier dein Username an </label>
-      <input className={styles.input} type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input className={styles.input} type="text" placeholder="UserName" value={newUserName} onChange={(e) => setNewUserName(e.target.value)} />
       <label>Bitte gib hier dein Vornamen an </label>
-      <input className={styles.input} type="text" placeholder="Vorname" value={firstName} onChange={(e) => setFirstName(e.target.value)} />  
+      <input className={styles.input} type="text" placeholder="Vorname" value={newVorName} onChange={(e) => setNewVorName(e.target.value)} />  
        <label>Bitte gib hier dein Nachname an </label>
-      <input className={styles.input} type="text" placeholder="Nachname" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+      <input className={styles.input} type="text" placeholder="Nachname" value={newNachName} onChange={(e) => setNewNachName(e.target.value)} />
       <label>Bitte gib hier deine E-Mail an </label>
-      <input className={styles.input} type="email" placeholder="E-Mail Adresse" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input className={styles.input} type="email" placeholder="E-Mail Adresse" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
       <label>Bitte gib hier dein Passwort an </label>
-      <input className={styles.input} type="password" placeholder="Passwort" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <input className={styles.input} type="password" placeholder="Passwort" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
       <label>Bitte bestätige dein Passwort nochmal </label>
-      <input className={styles.input} type="password" placeholder="Passwort" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <input className={styles.input} type="password" placeholder="Passwort wiederholen" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+      {error && <p>{error}</p>}
       <br />
       <div className={styles.button} >
-      <StandardBtn text={"Bestätigen"}onClick={handleConfirm} />
+      <StandardBtn  text={"Bestätigen"}onClick={handleSignup} />
       <StandardBtn text={"Abbrechen"}onClick={handleCancel} />
       </div>
       <br />
@@ -44,5 +63,5 @@ function SignUpPage() {
     </div>
   
   );
-}
+};
 export default SignUpPage;
