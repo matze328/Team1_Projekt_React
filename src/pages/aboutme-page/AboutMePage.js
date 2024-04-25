c01a80be7e8f8681bf90ac2ae95158576f9ba61c
banu-i <152266442+banu-i@users.noreply.github.com>
2024-04-25 10:47:50 +0200

    Update AboutMePage.js
    Update AboutMePage:
    About "Banu" - Content ergänzt.

--- src/pages/aboutme-page/AboutMePage.js
+++ src/pages/aboutme-page/AboutMePage.js
@@ -3,19 +3,18 @@ import React from "react";
 import AboutMeBox from "../../components/common/aboutMeBox";
 function AboutMePage() {
   return (
+    <div className={styles.mainContainer}>
+      <AboutMeBox
+        vorname="Marcel"
+        nachname="Büscher"
+        persoInfo="Ich bin ein leidenschaftlicher Entwickler und liebe es, kreative Lösungen zu finden."
+        hobbies="In meiner Freizeit fahre ich Downhill und Freeride."
+      />
 
-   <div className={styles.mainContainer}>  
-     <AboutMeBox 
-     vorname ="Marcel"
-     nachname="Büscher"
-     persoInfo= "Ich bin ein leidenschaftlicher Entwickler und liebe es, kreative Lösungen zu finden."
-     hobbies="In meiner Freizeit fahre ich Downhill und Freeride."
-     />
-     
-       <AboutMeBox    
-        vorname="Person 2"
-        nachname="This is about me"
-        hobbies="Hobby 3, Hobby 4"
+      <AboutMeBox
+        vorname="Banu"
+        persoInfo="Mit einer Begeisterung für Design und Ästhetik, strebe ich danach, visuell ansprechende, innovative und voll funktionierende Lösungen zu gestalten."
+        hobbies="Gerne gehe ich auf Städtereisen und trainiere im Fitnessstudio, um fit zu bleiben. Selbständige Weiterentwicklung durch Kurse und Workshops ist mir wichtig, um meine Fähigkeiten kontinuierlich zu verbessern."
       />
       <AboutMeBox
         vorname="Person 3"
@@ -30,8 +29,5 @@ function AboutMePage() {
     </div>
   );
 }
-
-
-
 
 export default AboutMePage;
