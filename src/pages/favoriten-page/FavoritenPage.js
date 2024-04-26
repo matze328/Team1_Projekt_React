6d8eaea7b938aafc7aa60674e0d2a3b9da4912da
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-04-26 17:18:22 +0200

    add content FavoritenPage
    - Video zu Favoriten eingebettet

--- src/pages/favoriten-page/FavoritenPage.js
+++ src/pages/favoriten-page/FavoritenPage.js
@@ -2,14 +2,21 @@ import styles from "./FavoritenPage.module.css";
 import SidebarHome from "../../components/layout/sidebar-home";
 
 function FavoritenPage() {
-
-
   return (
     <>
+      <SidebarHome />
       <div className={styles.mainContainer}>
-        <SidebarHome />
+        <iframe
+          width="914"
+          height="514"
+          src="https://www.youtube.com/embed/H55_66R72eg"
+          title="Happy 1st Birthday, Cammy! ^^"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
       </div>
- 
     </>
   );
 }
