e81117286caaf40f17a4567bf7394230e6f539fd
matze328 <152269645+matze328@users.noreply.github.com>
2024-10-03 16:08:57 +0200

    GET anfragen hinzugefügt mit Backend End Punkt verbunden
    -- init GET Anfrage fetchArtists ( gibt eine Liste aller einzigartigen Künstler zurück)
    - init GET Anfrage  fetchAlbumsByArtist  (gibt eine Liste aller Alben eines bestimmten Künstlers zurück)
    - init GET Anfrage fetchSongsByAlbumAndArtist ( gibt eine Liste aller Songs eines bestimmten Albums eines bestimmten Künstlers zurück)

--- src/api/v1/song/SongQueries.js
+++ src/api/v1/song/SongQueries.js
@@ -1,21 +1,45 @@
 import api from "../../config/api";
 
+// Funktion zum Abrufen aller Songs
 export async function fetchAllSong() {
-  const result = await api.get("/song/all");
-
-  const songs = result.data;
-  
-  return songs;
+    const result = await api.get("/song/all");
+    const songs = result.data;
+    return songs;
 }
 
-export async function fetchBySongId() {
-  const result = await api.get("/song/songid");
-
-  const reponse = result.data;
-
-  console.log("Mein Song By ID:", reponse);
-
-  return reponse;
+// Funktion zum Abrufen eines Songs anhand der SongID
+export async function fetchBySongId(songId) {
+    const result = await api.get(`/song/songid/${songId}`);
+    const response = result.data;
+    console.log("Mein Song By ID:", response);
+    return response;
 }
 
-export default { fetchAllSong, fetchBySongId };
+// Funktion zum Abrufen aller Künstler
+export async function fetchAllArtists() {
+    const result = await api.get("/song/artists");
+    const artists = result.data;
+    return artists;
+}
+
+// Funktion zum Abrufen aller Alben eines bestimmten Künstlers
+export async function fetchAlbumsByArtist(artist) {
+    const result = await api.get(`/song/albums/${artist}`);
+    const albums = result.data;
+    return albums;
+}
+
+// Funktion zum Abrufen aller Songs eines bestimmten Albums eines Künstlers
+export async function fetchSongsByAlbumAndArtist(artist, album) {
+    const result = await api.get(`/song/songs/${artist}/${album}`);
+    const songs = result.data;
+    return songs;
+}
+
+export default {
+    fetchAllSong,
+    fetchBySongId,
+    fetchAllArtists,
+    fetchAlbumsByArtist,
+    fetchSongsByAlbumAndArtist,
+};
